c52cb2a4267dd1aa79ffe1abcb61f395087b0bee
naghamghanim <naghamghanim@gmail.com>
2019-10-20 15:21:07 +0300

    Add html files

---
+++ HTML-CSS-JS/js/test.js
@@ -0,0 +1,7 @@
+// this is comment
+function ChangeColor()
+{
+  document.getElementById("id1").className="h2blue";
+  document.getElementById("id2").className="h2blue";
+  document.getElementById("id3").className="h2blue";
+}
