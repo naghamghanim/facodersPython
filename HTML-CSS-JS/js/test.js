978e3e26f942944ae0d970550bad64a11680b389
naghamghanim <naghamghanim@gmail.com>
2019-10-21 08:53:17 +0300

    Edited Files

--- HTML-CSS-JS/js/test.js
+++ HTML-CSS-JS/js/test.js
@@ -1,6 +1,9 @@
 // this is comment
 function ChangeColor()
 {
+//  alert("hello");
+//  var x=document.getElementById("id1").innerHTML;
+//  alert(x);
   document.getElementById("id1").className="h2blue";
   document.getElementById("id2").className="h2blue";
   document.getElementById("id3").className="h2blue";
